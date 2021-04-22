75aea14713aa345cfb839aa8aac855764a87a51c
mbrecht <brecht.michael.b@gmail.com>
2021-04-22 15:21:43 -0600

    removes phone number

--- components/Contact.js
+++ components/Contact.js
@@ -6,16 +6,13 @@ export default function Contact() {
   return (
     <div className={styles.container} id="contact">
       <h3 className={styles.title}>Contact Me</h3>
-      <p className={styles.subtitle}>
-        (PS: I'm a software engineer, this isn't a real restaurant)
-      </p>
-      <p className={styles.phone}>
-        <Link href="tel:5558675309">(555) 867-5309</Link>
-      </p>
       <p className={styles.email}>
         <Link href="mailto:brecht.michael.b@gmail.com">
           brecht.michael.b@gmail.com
         </Link>
+      </p>
+      <p className={styles.subtitle}>
+        (PS: I'm a software engineer, this isn't a real restaurant)
       </p>
     </div>
   );
